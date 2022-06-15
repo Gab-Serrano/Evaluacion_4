49ec650b1917112d80e11cea5a313b044ae585d9
wolfm4te <da.mallma@duocuc.cl>
2022-06-14 21:10:51 -0400

    Update

--- funciones.py
+++ funciones.py
@@ -1,6 +1,14 @@
 datos=[]
 #Función grabar
-def grabar(nif,nom,edad,fech,ueur,conyu):
+def grabar(nif,nom,edad,fech,conyu):
+    if conyu == 1:
+        conyu="Soltero"
+    elif conyu == 2:
+        conyu="Casado"
+    elif conyu == 3:
+        conyu="Divorciado"
+    else:
+        conyu="Viudo"
     datos.append([nif,nom,edad,fech,conyu])
 #Función buscar
 def buscar(nif):
@@ -12,7 +20,7 @@ def buscar(nif):
                 if count == 1:
                     print("Número NIF:",j)
                 elif count == 2:
-                    print("Nombre",j)
+                    print("Nombre:",j)
                 elif count == 3:
                     print("Edad:",j)
                 elif count == 4:
@@ -23,8 +31,58 @@ def buscar(nif):
             print("La persona buscada no pertence a la Unión Europea.")
 
 #Función imprimir
-def imprimir():
-    pass
+def imprimir(submenu,nif):
+    if submenu == 1:
+        for i in datos:
+            if nif in i:
+                count=0
+                print("*"*3,"Certificado de Nacimiento","*"*3)
+                for j in i:
+                    count+=1
+                    if count == 1:
+                        print("Número NIF:",j)
+                    elif count == 2:
+                        print("Nombre:",j)
+                    elif count == 4:
+                        print("Fecha de nacimiento:",j)
+                print("")
+                print("Firma: Registro Civil de Andalucía")
+    elif submenu == 2:
+        for i in datos:
+            if nif in i:
+                count=0
+                print("*"*3,"Certificado de estado conyugal","*"*3)
+                print("")
+                for j in i:
+                    count+=1
+                    if count == 1:
+                        nif_n=j
+                    elif count == 2:
+                        nombr=j
+                    elif count == 5:
+                        es_civ=j
+                print("La persona de nombre",nombr,", número NIF",nif_n,"mantiene un estado civil",es_civ,".")
+                print("")
+                print("Firma: Registro Civil de Andalucía")
+    else:
+        for i in datos:
+            if nif in i:
+                count=0
+                print("*"*3,"Certificado de Pertenencia a la Unión Europea","*"*3)
+                print("")
+                for j in i:
+                    for j in i:
+                        count+=1
+                        if count == 1:
+                            nif_n=j
+                        elif count == 2:
+                            nombr=j
+                        elif count == 3:
+                            edad=j
+                print("La persona de nombre",nombr,", número NIF",nif_n,"de",edad,"años")
+                print("Pertenece a la Unióin Europea ya que posee un NIF válido.")
+                print("")
+                print("Firma: Registro Civil de Andalucía")
 
 #Verificar NIF
 def verif_nif(nif,val=0):
--- principal.py
+++ principal.py
@@ -27,7 +27,7 @@ while True:
                 print("Debe ingresar solo números.")
         fech_nac=input("Ingrese la fecha de nacimiento (dd/mm/aa):\n")
         conyu=input("¿Cuál es su estado civil?\n1. Soltero.\n2. Casado.\n3. Divorciado.\n4. Viudo.\n")
-        fn.grabar(nif_num,nombre,edad,fech_nac,ueur,conyu)
+        fn.grabar(nif_num,nombre,edad,fech_nac,conyu)
     elif menu == 2: #Buscar
         while True:
             buscar_nif=input("Ingrese el NIF de la persona a buscar:\n")
@@ -37,8 +37,22 @@ while True:
                 print("Debe ingresar un NIF correcto XXXXXXXX-ABC.")
         fn.buscar(buscar_nif)
     elif menu == 3: #Imprimir
-        pass
-        fn.imprimir()
+        while True:
+            try:
+                option=int(input("¿Qué tipo de certificado desea imprimir?\n1. Certificado de nacimiento.\n2. Certificado de estado conyugal.\n3. Certificado de pertenencia a la Unión Europea.\n"))
+                if option > 0 and option < 4:
+                    break
+                else:
+                    print("Ingrese una opción correcta.")           
+            except ValueError:
+                print("Ingrese solo números.")
+        while True:
+            impr_nif=input("Ingrese el NIF de la persona:\n")
+            if fn.verif_nif(impr_nif):
+                break
+            else:
+                print("Debe ingresar un NIF correcto XXXXXXXX-ABC.")
+        fn.imprimir(option,impr_nif)
     elif menu == 4: #Salir
         break
     else:
